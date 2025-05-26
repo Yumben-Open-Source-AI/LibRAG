df998384b53421425b2593b9fba40202348d323d
Xforeverm <1296754116@qq.com>
2025-05-26 16:29:46 +0800

    fix rag-app:调整db连接池

--- rag-app/db/database.py
+++ rag-app/db/database.py
@@ -8,7 +8,12 @@ sqlite_file_name = "librag.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}?charset=utf8"
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_engine(
+    sqlite_url,
+    connect_args=connect_args,
+    pool_size=30,
+    pool_timeout=30,
+)
 
 
 def get_session():
