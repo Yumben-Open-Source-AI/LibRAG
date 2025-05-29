0da10e9a3c85af35225bfadce497f91c66dbf892
xqm <xqm@yumben.com>
2025-05-29 17:22:52 +0800

    fix rag-app:调整main

--- rag-app/main.py
+++ rag-app/main.py
@@ -6,6 +6,8 @@ from db.database import create_db_and_tables
 from fastapi.middleware.cors import CORSMiddleware
 from web_server.ai.router import router as ai_router
 from web_server.dify.router import router as dify_router
+from tools.log_tools import manage_logger as logger
+from multiprocessing import cpu_count
 
 app = FastAPI()
 app.include_router(ai_router)
@@ -32,7 +34,7 @@ async def log_request(request: Request, call_next):
     query_params = dict(request.query_params)  # 获取查询参数
 
     # 打印开始日志
-    print(f"\n[START] 开始时间: {start_time} | 请求路径: {path} | 参数: {query_params}")
+    logger.debug(f'请求路径: {path} | 参数: {query_params}')
 
     try:
         response = await call_next(request)
@@ -40,7 +42,7 @@ async def log_request(request: Request, call_next):
         # 确保无论是否异常都记录结束时间
         end_time = datetime.datetime.now()
         duration = end_time - start_time
-        print(f"[END] 结束时间: {end_time} | 请求时长: {duration.total_seconds():.2f}s")
+        logger.debug(f'请求时长: {duration.total_seconds():.2f}s')
 
     return response
 
@@ -52,4 +54,11 @@ def on_startup():
 
 if __name__ == '__main__':
     load_dotenv()
-    uvicorn.run(app='main:app', host='0.0.0.0', port=13113, workers=1)
+    uvicorn.run(
+        app='main:app',
+        host='0.0.0.0',
+        port=13113,
+        workers=cpu_count() * 2,
+        timeout_keep_alive=3,  # 指定3s内保持活动状态的连接
+        backlog=4096  # 等待处理最大连接数
+    )
